597fd276c9a309d98c26f793cf56c070290bb412
Allister <ageebernal@aol.com>
2020-10-18 14:30:19 -0700

    add navigation and icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import AppRouter from './router/AppRouter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+        <AppRouter />
     </div>
   );
 }
