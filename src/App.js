b1db7911473062cf56e25ebef30551a92b5dc26b
Allister <ageebernal@aol.com>
2020-10-18 15:12:29 -0700

    add browserrouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
+import AppRouter from './router/AppRouter';
 
 function App(props) {
     return (
         <>
-            <h1>Hello</h1>
+            <AppRouter />
         </>
     )
 }
---
+++ src/router/AppRouter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function AppRouter() {
+    return (
+        <BrowserRouter>
+            <Switch>
+
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default AppRouter;
