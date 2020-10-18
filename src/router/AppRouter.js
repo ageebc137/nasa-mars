bc7fee041ea8c2ccd9425b6837d24121fbf5bdab
Allister <ageebernal@aol.com>
2020-10-18 14:11:35 -0700

    add router

---
+++ src/router/AppRouter.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
+function AppRouter() {
+    return (
+        <BrowserRouter>
+        </BrowserRouter>
+    )
+}
